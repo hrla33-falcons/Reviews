cda3189ecc97fbe2df55fe10592406536531f7a9
Matthew Ginther <mg78856@gmail.com>
2019-12-14 14:56:36 -0800

    updated styling

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -14,7 +14,8 @@ const Map = ({ zip }) => {
     overflowWrap: 'break-word',
     textSizeAdjust: '100%',
     wordBreak: 'break-word',
-    fontFamily: 'Lato'
+    fontFamily: 'Lato',
+    marginBottom: '16px'
   };
 
   var map_footer = {
@@ -27,25 +28,31 @@ const Map = ({ zip }) => {
     fontFamily: 'Lato',
     marginLeft: '6px',
     lineHeight: '1.125em',
-  }
+  };
+
+  var map_iframe_container = {
+    minHeight: '300px',
+    maxHeight: '500px',
+    height: '65vh'
+  };
 
   console.log('this is address obj: ' + addressObj)
   return (
     <div>
       <h2>
-        <strong>
-          <span style={map_title}>
-            {`Map`}
-          </span>
-        </strong>
+        <span style={map_title}>
+          {`Map`}
+        </span>
       </h2>
-      <iframe
-        width={'100%'}
-        height={'600'}
-        frameBorder={0} style={{border: 0, zindex: -1, border: 'none'}}
-        src={`https://www.google.com/maps/embed/v1/place?key=${foo.map}
-          &q=${addressObj.city}+${addressObj.country}`} allowFullScreen>
-      </iframe>
+      <div style={map_iframe_container}>
+        <iframe
+          width={'100%'}
+          height={'100%'}
+          frameBorder={0} style={{border: 0, zindex: -1, border: 'none'}}
+          src={`https://www.google.com/maps/embed/v1/place?key=${foo.map}
+            &q=${addressObj.city}+${addressObj.country}`} allowFullScreen>
+        </iframe>
+      </div>
       <div style={{marginTop: '6px'}}>
         <span>
           <GoLocation color={'#717171'} size={12} />
